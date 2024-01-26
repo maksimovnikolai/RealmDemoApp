87e5262f22aae005c8bb98b99986886c076b2809
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-01-26 13:21:29 +0300

    add model - taskList

---
+++ RealmDemo/RealmDemo/Models/TaskList.swift
@@ -0,0 +1,22 @@
+//
+//  TaskList.swift
+//  RealmDemo
+//
+//  Created by Nikolai Maksimov on 26.01.2024.
+//
+
+import Foundation
+import RealmSwift
+
+class TaskList: Object {
+    @Persisted var name = ""
+    @Persisted var date = Date()
+    @Persisted var tasks = List<Task>()
+}
+
+class Task: Object {
+    @Persisted var name = ""
+    @Persisted var note = ""
+    @Persisted var date = Date()
+    @Persisted var isCompleted = false
+}
