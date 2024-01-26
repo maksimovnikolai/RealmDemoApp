69ce07afca4a353a5ffd45cbe0b0ac7d1dfaa3fd
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-01-26 16:37:16 +0300

    add storage manager - realm (CRUD)

---
+++ RealmDemo/RealmDemo/Services/StorageManager.swift
@@ -0,0 +1,85 @@
+//
+//  StorageManager.swift
+//  RealmDemo
+//
+//  Created by Nikolai Maksimov on 26.01.2024.
+//
+
+import Foundation
+import RealmSwift
+
+final class StorageManager {
+    
+    static let shared = StorageManager()
+    let realm = try! Realm()
+    
+    private init() {}
+    
+    // MARK: - Task List
+    func save(_ taskLists: [TaskList]) {
+        write {
+            realm.add(taskLists)
+        }
+    }
+    
+    func save(_ taskList: TaskList) {
+        write {
+            realm.add(taskList)
+        }
+    }
+    
+    func delete(_ taskList: TaskList) {
+        write {
+            realm.delete(taskList.tasks)
+            realm.delete(taskList)
+        }
+    }
+    
+    func edit(_ taskList: TaskList, newValue: String) {
+        write {
+            taskList.name = newValue
+        }
+    }
+
+    func done(_ taskList: TaskList) {
+        write {
+            taskList.tasks.setValue(true, forKey: "isComplete")
+        }
+    }
+
+    // MARK: - Tasks
+    func save(_ task: Task, to taskList: TaskList) {
+        write {
+            taskList.tasks.append(task)
+        }
+    }
+    
+    func delete(_ task: Task) {
+        write {
+            realm.delete(task)
+        }
+    }
+    
+    func rename(_ task: Task, to name: String, withNote note: String) {
+        write {
+            task.name = name
+            task.note = note
+        }
+    }
+    
+    func done(_ task: Task) {
+        write {
+            task.isCompleted.toggle()
+        }
+    }
+    
+    private func write(completion: () -> Void) {
+        do {
+            try realm.write {
+                completion()
+            }
+        } catch {
+            print(error)
+        }
+    }
+}
