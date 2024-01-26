18d0d367302f0c2e1ba978cbb27ae3c2ad3a0f8d
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-01-26 12:53:13 +0300

    setup project

---
+++ RealmDemo/RealmDemo/ViewControllers/TaskListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TaskListViewController.swift
+//  RealmDemo
+//
+//  Created by Nikolai Maksimov on 26.01.2024.
+//
+
+import UIKit
+
+final class TaskListViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+}
